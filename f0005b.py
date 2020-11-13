d49f34b0189b93b1cb1b3930c80e72d064fe3ac2
hadaszibalazs20 <hadaszi.balazs012345@gmail.com>
2020-11-13 08:29:02 +0000

    Initial commit

---
+++ f0005b.py
@@ -0,0 +1,6 @@
+jelszó = '(*Jelszó=('
+tipp = input('Mi a jelszó? ')
+if tipp == jelszó:
+    print('Bemehet.')
+else:
+    print('Hozzáférés megtagadva.')
---
+++ f0005c.py
@@ -0,0 +1,9 @@
+sz1 = 50
+sz2 = 15
+
+tipp = int(input('Mennyi ' + '+' + str(sz2) + ' ? '))
+
+if tipp == sz1 + sz2:
+    print('Nem rossz, nem rossz...')
+else:
+    print('Ejnye!')
